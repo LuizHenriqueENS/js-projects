60d73e3456cb5f3cd26e45dd0ce651d30d28d6d2
LuizHenriqueENS <luizhenrique.egydio@gmail.com>
2024-10-30 17:27:45 -0300

    todo project: edit e remove button

--- todo-app-js/script.js
+++ todo-app-js/script.js
@@ -1,25 +1,68 @@
-let taskContainer = document.getElementById('container')
-let i = 0
-
+let taskContainer = document.getElementById('task')
 let listOfTask = []
+
+
+// function addTask(){
+//     let createTask = document.createElement('tr')
+//     let createTD = document.createElement('td')
+//     let taskName = document.createElement('td')
+//     let taskDelete = document.createElement('button')
+//     let taskEdit = document.createElement('button')
+//     taskName.innerHTML = document.getElementById('task-field').value
+
+//     taskContainer.appendChild(createTask)
+
+//     createTask.appendChild(taskName)
+//     createTask.appendChild(taskDelete)
+//     createTask.appendChild(taskEdit)
+    
+//     taskDelete.setAttribute('onclick', 'getTableIndex(this.cellIndex)')
+// }
+
+function getTableIndex(element){
+    alert(element)
+}
 
 function createTask(){
     let new_task = document.createElement('div')
-    let newTaskDeleteButton = document.createElement('button')
-    let newTaskEditButton = document.createElement('button')
-    
-    newTaskDeleteButton.innerHTML = "delete"
-    newTaskDeleteButton.setAttribute('onclick', 'alertTest(this.parentElement.id)')
-    new_task.setAttribute("id", `${i}`)
-    newTaskEditButton.innerHTML = "edit"
-    new_task.innerHTML = `${i}`
+    let newTaskDeleteButton = creatDeleteButton()
+    let newTaskEditButton = createEditButton()
+
+    createTaskCard(new_task, newTaskDeleteButton, newTaskEditButton)
+}
+
+function createTaskCard(new_task, newTaskDeleteButton, newTaskEditButton) {
+    let inputField = document.getElementById('task-field')
+    new_task.setAttribute("id", `${taskContainer.childElementCount}`)
+    new_task.innerHTML = `${inputField.value}`
+    inputField.value = ''
     taskContainer.appendChild(new_task)
     new_task.appendChild(newTaskDeleteButton)
     new_task.appendChild(newTaskEditButton)
-    i++
-    listOfTask.push(new_task)
+    
 }
 
-function alertTest(id){
-    
+function creatDeleteButton() {
+    let newTaskDeleteButton = document.createElement('button')
+    newTaskDeleteButton.innerHTML = "delete"
+    newTaskDeleteButton.setAttribute('onclick', 'removeTask(this.parentNode)')
+    return newTaskDeleteButton
+}
+
+function createEditButton() {
+    let newTaskEditButton = document.createElement('button')
+    newTaskEditButton.innerHTML = "edit"
+    newTaskEditButton.setAttribute('onclick', 'editTask(this.parentElement.id)')
+    return newTaskEditButton
+}
+
+function removeTask(id){
+    let confirmDelete = confirm("Deseja mesmo deletar a tarefa?")
+    if(confirmDelete){
+        taskContainer.removeChild(id)
+    }
+}
+
+function editTask(parentID){
+    alert(`O ID é: ${parentID}`)
 }
